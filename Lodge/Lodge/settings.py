e552ddf9890ee58d9cf92adec05b8b75fda55398
isaacenobun <76211324+isaacenobun@users.noreply.github.com>
2024-06-23 10:58:39 +0100

    Changed db

--- Lodge/Lodge/settings.py
+++ Lodge/Lodge/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 
 import os
 
-import dj_database_url
+# import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -78,16 +78,16 @@ WSGI_APPLICATION = 'Lodge.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
-    'default' : dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
+# DATABASES = {
+#     'default' : dj_database_url.parse(os.environ.get('DATABASE_URL'))
+# }
 
 
 # Password validation
