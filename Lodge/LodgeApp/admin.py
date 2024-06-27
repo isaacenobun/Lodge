90f30d484c95aa516a9eed1d8c270609b26988d6
aayooluwa <ayooluwaakinola@gmail.com>
2024-06-27 18:16:58 +0100

    Revert "Update to models and views.py"
    This reverts commit 7cc3fa073de8c51d526a95b7ff12ecf9d1d8f926.

--- Lodge/LodgeApp/admin.py
+++ Lodge/LodgeApp/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Staff, Room, Guest, Log, Revenue, CheckIns, Company, Suite
+from .models import *
 
 # Register your models here.
 admin.site.register(Staff)
@@ -8,5 +8,3 @@ admin.site.register(Guest)
 admin.site.register(Log)
 admin.site.register(Revenue)
 admin.site.register(CheckIns)
-admin.site.register(Company)
-admin.site.register(Suite)
--- Lodge/LodgeApp/views.py
+++ Lodge/LodgeApp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse
 from django.views.decorators.csrf import csrf_exempt
 
-from .models import Staff, Room, Guest, Log, Revenue, CheckIns, Company, Suite
+from .models import Staff, Room, Guest, Log, Revenue, CheckIns
 
 from django.contrib.auth import login, authenticate, logout, get_user_model
 
@@ -12,9 +12,6 @@ from datetime import datetime, date, timedelta
 from django.utils import timezone
 
 import numpy as np
-import re
-
-from collections import defaultdict
 
 # Create your views here.
 def sign_in(request):
@@ -52,15 +49,10 @@ def dashboard(request):
     if not request.user.is_authenticated:
         return redirect('sign-in')
     
-    logs = Log.objects.filter(
-        timestamp__date=timezone.now().date(),
-        company=request.user.company
-        ).order_by('-id')[:5]
-    active_guests = Guest.objects.filter(check_out__isnull=True,
-                                         company=request.user.company)
-    total_guests = Guest.objects.filter(company=request.user.company).count()
-    available_rooms = Room.objects.filter(room_status=False,
-                                          company=request.user.company).count()
+    logs = Log.objects.filter(timestamp__date=timezone.now().date()).order_by('-id')[:5]
+    active_guests = Guest.objects.filter(check_out__isnull=True)
+    total_guests = Guest.objects.count()
+    available_rooms = Room.objects.filter(room_status=False).count()
     
     context = {
         'active_guests': active_guests,
@@ -76,31 +68,29 @@ def rooms(request):
     if not request.user.is_authenticated:
         return redirect('sign-in')
     
-    suite_types = Suite.objects.values_list('type', flat=True).distinct()
+    exec_rooms = Room.objects.filter(room_type='executive')
+    bus_rooms = Room.objects.filter(room_type='business')
+    reg_rooms = Room.objects.filter(room_type='regular')
+    guests = Guest.objects.filter(check_out__isnull=True)
     
-    suite_types_dict = {}
-    for suite_type in suite_types:
-        suite_rooms = Room.objects.filter(suite__type=suite_type, company=request.user.company)
-        suite_types_dict[suite_type] = suite_rooms
-        
-    guests = Guest.objects.filter(check_out__isnull=True, company=request.user.company)
     room_guest_mapping = {guest.room.id: guest for guest in guests}
     
     context = {
-        'suite_types': suite_types_dict,
+        'exec_rooms': exec_rooms,
+        'bus_rooms': bus_rooms,
+        'reg_rooms': reg_rooms,
         'room_guest_mapping': room_guest_mapping,
         'check_out_url': reverse('check-out'),
         'check_in_url': reverse('check-in'),
         'page_name':'Rooms'
     }
-    
     return render(request, 'room-carousel.html', context)
 
 def history(request):
     if not request.user.is_authenticated:
         return redirect('sign-in')
     
-    guests = Guest.objects.filter(company=request.user.company)
+    guests = Guest.objects.all()
     context = {'guests': guests, 'page_name':'History'}
     return render(request, 'guest-history.html', context)
 
@@ -117,8 +107,7 @@ def check_in(request):
             email=request.POST.get('email'),
             number=request.POST.get('phone'),
             room=room,
-            staff=request.user,
-            company=request.user.company
+            staff=request.user
         )
 
         room.room_status = True
@@ -128,13 +117,10 @@ def check_in(request):
             staff=request.user,
             check_in_action=f'{request.user} checked in {guest.name} into Room {guest.room.room_number}',
             check_status=True, 
-            timestamp=guest.check_in,
-            company=guest.company
+            timestamp=guest.check_in
         )
         
-        CheckIns.objects.create(
-            company=guest.staff.company
-        )
+        CheckIns.objects.create()
 
         return redirect('dashboard')
     
@@ -156,7 +142,7 @@ def check_out(request):
             guest.save()
             
             revenue_amount = guest.room.room_price * (guest.check_out - guest.check_in).days
-            revenue = Revenue.objects.create(revenue=revenue_amount, company=request.user.company)
+            revenue = Revenue.objects.create(revenue=revenue_amount)
             
             guest.revenue = revenue
             guest.save()
@@ -168,67 +154,21 @@ def check_out(request):
                 staff=request.user,
                 check_out_action=f'{request.user} checked out {guest.name} from Room {guest.room.room_number}',
                 check_status=False, 
-                timestamp=guest.check_out,
-                company=request.user.company
+                timestamp=guest.check_out
             )
         return redirect('dashboard')
     
     return redirect('sign-in')
 
-@csrf_exempt
 def onboarding(request):
-    if request.method == 'POST':
-        company_name = request.POST.get('company_name')
-        
-        company = Company.objects.create(name=company_name)
-        
-        suite_type_pattern = re.compile(r'suite_type_\d+')
-        suite_types = [value for key, value in request.POST.items() if suite_type_pattern.match(key)]
-        
-        suite_price_pattern = re.compile(r'suite_price_\d+')
-        suite_prices = [value for key, value in request.POST.items() if suite_price_pattern.match(key)]
-        
-        suite_rooms_pattern = re.compile(r'suite_rooms_\d+')
-        suite_rooms = [value for key, value in request.POST.items() if suite_rooms_pattern.match(key)]
-        
-        room_tag_pattern = re.compile(r'room_tag_(\d+)_\d+')
-        room_tags_grouped = defaultdict(list)
-        for key, value in request.POST.items():
-            match = room_tag_pattern.match(key)
-            if match:
-                suite_number = int(match.group(1))
-                room_tags_grouped[suite_number].append((key, value))
-        
-        for iter in range(len(suite_types)):
-            suite_type = suite_types[iter]
-            suite_price = suite_prices[iter]
-            
-            new_suite = Suite.objects.create(
-                company=company,
-                type=suite_type,
-                price=suite_price
-            )
-            
-            for room in room_tags_grouped[iter + 1]:
-                room_tag = room[1]
-                new_room = Room.objects.create(
-                    suite=new_suite,
-                    company=company,
-                    room_number=room_tag
-                )
-            
-        return redirect('sign-in')
-    
     return render(request, 'onboarding.html')
 
 def analytics(request):
-    if not request.user.is_authenticated:
-        return redirect('sign-in')
     
     top_guests = Guest.objects.all().order_by('-id')[:5]
     guests = Guest.objects.all()
     
-    check_ins = CheckIns.objects.filter(time__year=timezone.now().year, company=request.user.company)
+    check_ins = CheckIns.objects.filter(time__year=timezone.now().year)
     year_dict = {i:0 for i in range(1,13)}
     for check_in in check_ins:
         month = check_in.time.month
@@ -255,10 +195,10 @@ def analytics(request):
         
     monthly_revenue = (total_revenue/(timezone.now().month))/1000
     
-    month_revenue_guests = Guest.objects.filter(check_out__month=timezone.now().month, company=request.user.company)
+    month_revenue_guests = Guest.objects.filter(check_out__month=timezone.now().month)
     
     if timezone.now().month != 1:
-        prev_month_revenue_guests = Guest.objects.filter(check_out__month=(timezone.now().month)-1, company=request.user.company)
+        prev_month_revenue_guests = Guest.objects.filter(check_out__month=(timezone.now().month)-1)
     else:
         prev_month_revenue_guests = {}
     
