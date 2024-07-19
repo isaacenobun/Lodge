1950d7edcad43c4856e51cb0b9445e3802836835
aayooluwa <ayooluwaakinola@gmail.com>
2024-07-19 23:27:51 +0100

    "Updated Lodge app: modified CSS files, added new JavaScript files, updated HTML templates, and changed database schema."

--- Lodge/LodgeApp/static/js/room-carousel.js
+++ Lodge/LodgeApp/static/js/room-carousel.js
@@ -15,6 +15,111 @@ function updateUI(selectedSuite) {
 }
 
 
+
+document.addEventListener('DOMContentLoaded', function () {
+  const selectElement = document.getElementById('suite');
+  const showcaseElement = document.querySelector('.showcase');
+  const editButton = document.createElement('button');
+  const saveButton = document.createElement('button');
+  const modalElement = document.getElementById('verticalycentered');
+
+  // Prevent the modal from showing if it is disabled
+  modalElement.addEventListener('show.bs.modal', function (event) {
+    if (modalElement.classList.contains('disabled')) {
+      event.preventDefault();
+    }
+  });
+
+  let originalContent = {};
+
+  function disableModal() {
+    modalElement.classList.add('disabled');
+  }
+
+  // Function to enable the modal
+  function enableModal() {
+    modalElement.classList.remove('disabled');
+  }
+
+  editButton.type = 'button';
+  editButton.id = 'editButton'
+  editButton.className = 'btn btn-link';
+  editButton.textContent = 'Edit SSSStags';
+  editButton.style.display = 'none'; // Initially hidden
+
+  saveButton.type = 'button';
+  saveButton.className = 'btn btn-link';
+  saveButton.textContent = 'Save Changes';
+  saveButton.style.display = 'none'; // Initially hidden
+
+  selectElement.addEventListener('change', function () {
+    if (selectElement.value) {
+      editButton.style.display = 'block';
+    } else {
+      editButton.style.display = 'none';
+      saveButton.style.display = 'none';
+      resetContent();
+    }
+  });
+
+  editButton.addEventListener('click', function () {
+    disableModal();
+    saveButton.style.display = 'inline-block';
+    document.body.classList.add('no-hover');
+
+    document.querySelectorAll('.room-tag').forEach(div => {
+      originalContent[div.dataset.id] = div.innerHTML;
+      div.contentEditable = true;
+      div.classList.add('editable');
+    });
+  });
+
+  saveButton.addEventListener('click', function () {
+
+    saveButton.style.display = 'none';
+    document.body.classList.remove('no-hover');
+    document.querySelectorAll('.room').forEach(div => {
+      div.classList.remove('no-click');
+    });
+    document.querySelectorAll('.room-tag').forEach(div => {
+      div.contentEditable = false;
+      div.classList.remove('editable');
+    });
+    enableModal();
+  });
+
+  document.querySelectorAll('.room-tag').forEach((div, index) => {
+    div.dataset.id = index; // Assign a unique ID to each div
+    div.addEventListener('contextmenu', function (event) {
+      event.preventDefault();
+      div.contentEditable = true;
+      div.classList.add('editable');
+    });
+  });
+
+  function resetContent() {
+    document.body.classList.remove('no-hover');
+    document.querySelectorAll('.room').forEach(div => {
+      div.classList.remove('no-click');
+    });
+    document.querySelectorAll('.room-tag').forEach(div => {
+      div.innerHTML = originalContent[div.dataset.id];
+      div.contentEditable = false;
+      div.classList.remove('editable');
+    });
+  }
+
+  showcaseElement.parentNode.insertBefore(editButton, showcaseElement.nextSibling);
+  showcaseElement.parentNode.insertBefore(saveButton, showcaseElement.nextSibling);
+});
+
+
+
+
+
+
+
+
 // Function to show the modal with the appropriate message
 function showModalWithOccupancyStatus(roomNumber, isOccupied, guestId, guestName, guestCheckout, roomQueryset) {
   // Get the modal elements
@@ -56,7 +161,7 @@ function showModalWithOccupancyStatus(roomNumber, isOccupied, guestId, guestName
 }
 
 // Function to handle the Extend Stay button click
-function handleExtendStay(guestId,extendUrl) {
+function handleExtendStay(guestId, extendUrl) {
   const modal = document.getElementById('verticalycentered');
   const modalBody = modal.querySelector('.modal-body');
   const modalFooter = modal.querySelector('.modal-footer');
@@ -89,7 +194,7 @@ function cancelExtendStay() {
 }
 
 // Function to handle the Confirm button click
-function confirmExtendStay(guestId,extendUrl) {
+function confirmExtendStay(guestId, extendUrl) {
   const newStayDuration = document.getElementById('extendStayInput').value;
   const modal = document.getElementById('verticalycentered');
   const modalBody = modal.querySelector('.modal-body');
